f78a0fc0fbbfce6f7190830a3542eeadc13bea68
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-08-03 20:55:17 +0100

    Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description:
+    'A full stack Spotify clone in Next 13.4 with React, Stripe, Supabase, PostgreSQL and Tailwind CSS ',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
