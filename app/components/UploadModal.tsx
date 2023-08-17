62198b7414d482c66a7506d48f975ca714b7d052
aindriu80 <aindriu80@gmail.com>
2023-08-17 14:53:18 +0100

    Styling the Input

--- app/components/UploadModal.tsx
+++ app/components/UploadModal.tsx
@@ -1,23 +1,48 @@
 "use client";
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import Input from "./Input";
 
 const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const uploadModal = useUploadModal();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
   const onChange = (open: boolean) => {
     if (!open) {
-      // Reset the form
+      reset();
       uploadModal.onClose();
     }
   };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // Upload to supabase
+  };
+
   return (
     <Modal
-      title="Upload modal title"
-      description="Upload modal description"
+      title="Add a song"
+      description="Upload a mp3 file"
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      Upload Content
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+      </form>
     </Modal>
   );
 };
