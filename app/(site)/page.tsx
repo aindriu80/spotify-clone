88e2c950ab2b860bac1e06b0bae339b05ba61a24
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-08-20 21:07:29 +0100

    creating the songs page -iniital

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
+import getSongs from '@/actions/getSongs';
 import Header from "../components/Header";
 import ListItem from "../components/ListItem";
+import PageContents from './components/PageContents';
 
-export default function Home() {
+export const revalidate = 0
+
+export default async function Home() {
+  const songs = await getSongs()
+
   return (
     <div className="w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
       <Header>
@@ -20,7 +26,7 @@ export default function Home() {
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-semibold text-white">Newest Songs</h1>
         </div>
-        <div className="">List of Songs</div>
+      <PageContents songs={songs}/>
       </div>
     </div>
   );
---
+++ app/components/SongItem.tsx
@@ -0,0 +1,6 @@
+"use client"
+const SongItem = () => {
+	return ( <div>Song Item</div> );
+}
+ 
+export default SongItem;
