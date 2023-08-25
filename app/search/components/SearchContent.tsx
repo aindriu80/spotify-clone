15f5dadcad04a0d33ce05685465245f03bab961b
aindriu80 <aindriu80@gmail.com>
2023-08-25 21:05:59 +0100

    Building out search page

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,6 @@
+"use client"
+const SearchContent = () => {
+	return ( <div>Search Content</div> );
+}
+ 
+export default SearchContent;
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,10 +1,11 @@
-"use strict";
+"use client";
 
 import qs from 'query-string'
 
 import useDebounce from '@/hooks/useDebounce';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import Input from './Input';
 
 	
 
@@ -22,13 +23,15 @@ const SearchInput = () => {
 			url: '/search',
 			query: query
 		})
-	},[])
+	router.push(url)
+	},[debouncedValue,router])
 
 
 
 	return ( 
 
-		<div className="">Search Input</div>
+		<Input placeholder='What do you want to listen to?' value={value} onChange={(e)=>setValue(e.target.value)}/>
+		
 	);
 }
  
